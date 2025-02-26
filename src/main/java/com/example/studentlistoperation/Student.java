95860b18efa9a40d4318c95547268c6b31dab0d0
alveesarker <asahanaz305@gmail.com>
2025-02-26 23:34:12 +0600

    added fxid and event method to the controller class

---
+++ src/main/java/com/example/studentlistoperation/Student.java
@@ -0,0 +1,4 @@
+package com.example.studentlistoperation;
+
+public class Student {
+}
--- src/main/java/com/example/studentlistoperation/StudentListOperationController.java
+++ src/main/java/com/example/studentlistoperation/StudentListOperationController.java
@@ -1,7 +1,45 @@
 package com.example.studentlistoperation;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.input.GestureEvent;
+
+import java.time.LocalDate;
+import java.util.Date;
+
 public class StudentListOperationController {
     @javafx.fxml.FXML
+    private PasswordField passwordTextField;
+    @javafx.fxml.FXML
+    private TableColumn<Student, Integer> idTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn<Student, String> nameTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn<Student, String> majorTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn<Student, Boolean> genderTableColumn;
+    @javafx.fxml.FXML
+    private TextField nameTextField;
+    @javafx.fxml.FXML
+    private TextField idTextField;
+    @javafx.fxml.FXML
+    private DatePicker dateOfBirth;
+    @javafx.fxml.FXML
+    private ComboBox<String> mejorComboBox;
+    @javafx.fxml.FXML
+    private TableColumn<Student, LocalDate> dobTableColumn;
+    @javafx.fxml.FXML
+    private RadioButton femaleRadioButton;
+    @javafx.fxml.FXML
+    private TableView<Student> studentTableView;
+    @javafx.fxml.FXML
+    private RadioButton maleRadioButton;
+
+    @javafx.fxml.FXML
     public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void handleAddRow(ActionEvent actionEvent) {
     }
 }
